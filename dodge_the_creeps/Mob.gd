96d850d144193b02aff3fc4ae480b5fa5ec44ad2
Felipe Gabriel <ofelipegabriel321@gmail.com>
2020-02-13 17:00:08 -0300

    feat: ends game dodge_the_creeps

--- dodge_the_creeps/Mob.gd
+++ dodge_the_creeps/Mob.gd
@@ -5,8 +5,10 @@ export var max_speed = 250  # Maximum speed range.
 var mob_types = ["walk", "swim", "fly"]
 
 func _ready():
-	# randi() % n é a maneira padrão de obter um inteiro aleatório entre 0 e n-1.
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 
 func _on_Visibility_screen_exited():
+	queue_free()
+
+func _on_start_game():
 	queue_free()
--- dodge_the_creeps/Player.gd
+++ dodge_the_creeps/Player.gd
@@ -1,20 +1,16 @@
 extends Area2D
 
 signal hit
+
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
-
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
-func _process(delta):	
-	var velocity = Vector2()
-	
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -23,7 +19,6 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -31,24 +26,24 @@ func _process(delta):
 		$AnimatedSprite.stop()
 	
 	position += velocity * delta
-	
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
-		# caso ele esteja olhando para baixoa antes de ir pro lado agora
-		# ele vai olhar para cima
 		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
-# warning-ignore:unused_argument
-func _on_Player_area_entered(area):
-	hide() # o personagem desaparece quando ocorre o hit
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
 	emit_signal("hit")
-	# desativar a colisão do jogador para que não acione o sinal
-	# hit mais de uma vez.
 	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
